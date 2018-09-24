d34d6a5165f6ddb85c570ed0e153c43d65adb2bd
fddata <fraserjdouglas@gmail.com>
2018-09-24 20:31:30 +0100

    adds toner to printer constructor and consumes on print

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,18 +1,30 @@
 public class Printer {
-    private int numberOfSheetsRemaining;
+    private int sheetsRemaining;
+    private int tonerRemaining;
 
     public Printer(){
-        this.numberOfSheetsRemaining = 100;
+        this.sheetsRemaining = 100;
+        this.tonerRemaining = 1000;
     }
 
-    public int getSheets() {
-        return this.numberOfSheetsRemaining;
+    public int getSheetsRemaining() {
+        return this.sheetsRemaining;
     }
 
     public  void print(int numberOfSheets, int numberOfCopies) {
         int sheetsRequired = numberOfSheets * numberOfCopies;
-       if (this.numberOfSheetsRemaining >= sheetsRequired){
-           this.numberOfSheetsRemaining -= sheetsRequired;
+       if (this.sheetsRemaining >= sheetsRequired && this.tonerRemaining >= sheetsRequired){
+           this.sheetsRemaining -= sheetsRequired;
+           this.tonerRemaining -= sheetsRequired;
        }
+    }
+
+    public void refill(int sheetsAdded) {
+        this.sheetsRemaining += sheetsAdded;
+    }
+
+
+    public int getTonerRemaining() {
+        return tonerRemaining;
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -14,22 +14,43 @@ public class PrinterTest {
 
     @Test
     public void hasSheets(){
-        int result = printer.getSheets();
+        int result = printer.getSheetsRemaining();
         assertEquals(100, result);
     }
 
     @Test
     public void canPrintTrue(){
         printer.print(30, 3);
-        int remainingSheets = printer.getSheets();
+        int remainingSheets = printer.getSheetsRemaining();
         assertEquals(10, remainingSheets);
     }
 
     @Test
     public void canPrintFalse(){
         printer.print(30,4);
-        int remainingSheets = printer.getSheets();
-        assertEquals(100,100);
+        int remainingSheets = printer.getSheetsRemaining();
+        assertEquals(100,remainingSheets);
     }
 
+    @Test
+    public void canRefill(){
+        printer.print(30,3);
+        printer.refill(30);
+        int remainingSheets = printer.getSheetsRemaining();
+        assertEquals(40, remainingSheets);
+    }
+
+    @Test
+    public void hasToner(){
+        int remainingToner = printer.getTonerRemaining();
+        assertEquals(1000, remainingToner);
+    }
+
+
+    @Test
+    public void consumeTonerOnPrint(){
+        printer.print(25, 2);
+        int remainingToner = printer.getTonerRemaining();
+        assertEquals(950, remainingToner);
+    }
 }
